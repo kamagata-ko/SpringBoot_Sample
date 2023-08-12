44ff038a27796572ab983c36e328c4d5af0570e5
kamagata-ko <eighth.9512@gmail.com>
2023-08-12 15:50:26 +0900

    first commite

---
+++ src/main/java/com/example/SpringBoot_Sample/domain/Employee.java
@@ -0,0 +1,8 @@
+package com.example.SpringBoot_Sample.domain;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private int id;
+}
---
+++ src/main/java/com/example/SpringBoot_Sample/web/SampleCotroller.java
@@ -0,0 +1,30 @@
+package com.example.SpringBoot_Sample.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringBoot_Sample.domain.Employee;
+
+@Controller
+public class SampleCotroller{
+    @GetMapping("/")
+    String getHello() {
+        return "hello";
+    }
+
+    @GetMapping("/test")
+    String getHoge() {
+        return "hoge/test";
+    }
+
+    @PostMapping("/resultCtrl")
+    String postResult(@RequestParam("val") String responseVal, Model mdoel) {
+        mdoel.addAttribute("responseVal", responseVal);
+        return "result";
+    }
+}
